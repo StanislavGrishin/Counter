80add0e702517598b7d7203c75cda4f0cb65dbf5
Stas Grishin <stanislavgrs@yandex.ru>
2024-11-07 11:28:41 +0300

    Исправил приватность

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,29 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counterValueButton: UIButton!
+    @IBOutlet private weak var counterValueButton: UIButton!
     
-    @IBOutlet weak var counterValueLabel: UILabel!
+    @IBOutlet private weak var counterValueLabel: UILabel!
    
-    @IBOutlet weak var counterValueLabel2: UILabel!
+    @IBOutlet private weak var counterValueLabel2: UILabel!
     
-    @IBOutlet weak var plusValueButton: UIButton!
+    @IBOutlet private weak var plusValueButton: UIButton!
     
-    @IBOutlet weak var minusValueButton: UIButton!
+    @IBOutlet private weak var minusValueButton: UIButton!
     
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
     
-    @IBOutlet weak var historyTextView: UITextView!
-    
-    var count:Int = 0
-    var count2:Int = 0
-    let date = Date()
-    
-    func getDate() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "[dd-MM-yyyy || HH:mm:ss]"
-        return dateFormatter.string(from: Date())
-    }
+    @IBOutlet private weak var historyTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,18 +31,18 @@ class ViewController: UIViewController {
         historyTextView.text = "История изменений:\n"
     }
 
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func buttonDidTap(_ sender: Any) {
         count += 1
         counterValueLabel.text = "Значение счётчика: \(count)"
     }
     
-    @IBAction func buttonPlusTap(_ sender: Any) {
+    @IBAction private func buttonPlusTap(_ sender: Any) {
         count2 += 1
         counterValueLabel2.text = "Значение счётчика: \(count2)"
         historyTextView.text += "\(getDate()) Значение изменено на +1\n"
     }
     
-    @IBAction func buttonMinusTap(_ sender: Any) {
+    @IBAction private func buttonMinusTap(_ sender: Any) {
         
         if count2 > 0 {
             count2 -= 1
@@ -63,10 +53,20 @@ class ViewController: UIViewController {
         }
         
     }
-    @IBAction func buttonResetTap(_ sender: Any) {
+    @IBAction private func buttonResetTap(_ sender: Any) {
         count2 = 0
         counterValueLabel2.text = "Значение счётчика: \(count2)"
         historyTextView.text += "\(getDate()) Значение сброшено\n"
+    }
+    
+    var count:Int = 0
+    var count2:Int = 0
+    let date = Date()
+    
+    func getDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "[dd-MM-yyyy || HH:mm:ss]"
+        return dateFormatter.string(from: Date())
     }
     
 }
